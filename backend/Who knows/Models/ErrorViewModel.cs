cf7bb851eb2956e3f0fc1e317fa5aa15f3b7ec8f
BaiGanio <lubenkikov@gmail.com>
2017-11-29 21:42:48 +0200

    Included boilerplate .NET Core 2.0 MVC template.

---
+++ backend/Who knows/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Who_knows.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
